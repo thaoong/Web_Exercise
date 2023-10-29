2310c543ff34b50bcb55c9cf8c8109a3bc2f66ef
thaoong <thao4985@gmail.com>
2023-10-29 18:58:03 +0700

    update

---
+++ Angular_Code/my-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SecondComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
